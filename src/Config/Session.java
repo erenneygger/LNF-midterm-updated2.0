a05132f223b4695ab90d6d9ae7844130ddaa47d9
erenneygger <erenneygger@gmail.com>
2026-02-24 14:06:00 +0800

    Karun ragyud

---
+++ src/Config/Session.java
@@ -0,0 +1,10 @@
+package Config;
+
+/**
+ * @author USER27
+ */
+public class Session {
+    // 0 means no user is logged in. This is used for our security checks.
+    public static int userId = 0; 
+    public static String name; // Add this to store the user's name
+}
